ccd56cfe89298f665fb1f64ac0d0d2b19340cd81
Rouan <rmw@thoughtworks.com>
2015-06-11 10:19:46 +0200

    added summary chart

--- app/scripts/features/selfAssessment/selfAssessmentCtrl.js
+++ app/scripts/features/selfAssessment/selfAssessmentCtrl.js
@@ -5,5 +5,23 @@ angular.module('coachingApp')
     var request = {method: 'GET', url: 'data.json', data: {}};
     $http(request).then(function(result) {
       $scope.team = result.data.team;
+
+      $scope.team.forEach(function (person) {
+        person.summary = {
+          category: 'Summary',
+          ratings: [
+            {
+              data: []
+            }
+          ],
+          labels: []
+        };
+        person.assessments.forEach(function (assessment) {
+          var currentRating = assessment.ratings[assessment.ratings.length - 1];
+          var average = _.sum(currentRating.data) / currentRating.data.length;
+          person.summary.ratings[0].data.push(average);
+          person.summary.labels.push(assessment.category);
+        });
+      });
     });
   });
--- app/scripts/features/selfAssessment/spiderChartDirective.js
+++ app/scripts/features/selfAssessment/spiderChartDirective.js
@@ -9,27 +9,27 @@ angular.module('coachingApp')
         latest: '='
       },
       link: function (scope, elem) {
-        var previousRating = scope.assessment.ratings[scope.assessment.ratings.length - 2];
-        previousRating.fillColor = 'rgba(220,220,220,0.2)';
-        previousRating.strokeColor = 'rgba(220,220,220,1)';
-        previousRating.pointColor = 'rgba(220,220,220,1)';
-        previousRating.pointStrokeColor = '#fff';
-        previousRating.pointHighlightFill = '#fff';
-        previousRating.pointHighlightStroke = 'rgba(220,220,220,1)';
-
-        var currentRating = scope.assessment.ratings[scope.assessment.ratings.length - 1];
-        currentRating.fillColor = 'rgba(151,187,205,0.2)';
-        currentRating.strokeColor = 'rgba(151,187,205,1)';
-        currentRating.pointColor = 'rgba(151,187,205,1)';
-        currentRating.pointHighlightStroke = 'rgba(151,187,205,1)';
-        currentRating.pointStrokeColor = '#fff';
-        currentRating.pointHighlightFill = '#fff';
-
         var ratings = [];
+        
+        var previousRating = scope.assessment.ratings[scope.assessment.ratings.length - 2];
         if (previousRating) {
+          previousRating.fillColor = 'rgba(220,220,220,0.2)';
+          previousRating.strokeColor = 'rgba(220,220,220,1)';
+          previousRating.pointColor = 'rgba(220,220,220,1)';
+          previousRating.pointStrokeColor = '#fff';
+          previousRating.pointHighlightFill = '#fff';
+          previousRating.pointHighlightStroke = 'rgba(220,220,220,1)';
           ratings.push(previousRating);
         }
+
+        var currentRating = scope.assessment.ratings[scope.assessment.ratings.length - 1];
         if (currentRating) {
+          currentRating.fillColor = 'rgba(151,187,205,0.2)';
+          currentRating.strokeColor = 'rgba(151,187,205,1)';
+          currentRating.pointColor = 'rgba(151,187,205,1)';
+          currentRating.pointHighlightStroke = 'rgba(151,187,205,1)';
+          currentRating.pointStrokeColor = '#fff';
+          currentRating.pointHighlightFill = '#fff';
           ratings.push(currentRating);
         }
 
