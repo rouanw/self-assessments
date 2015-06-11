4f518a30dc1c61ac65b90624de372679bbb676bb
Rouan <rmw@thoughtworks.com>
2015-06-11 16:40:28 +0200

    refactored teams so that logic sits in factory

--- app/scripts/features/selfAssessment/selfAssessmentCtrl.js
+++ app/scripts/features/selfAssessment/selfAssessmentCtrl.js
@@ -4,13 +4,8 @@ angular.module('coachingApp')
   .controller('SelfAssessmentCtrl', function ($scope, SelfAssessment) {
     SelfAssessment.getPeople().then(function (people) {
       $scope.people = people;
-      $scope.teamNames = _.uniq(_.map($scope.people, function (person) {
-        return person.currentTeam;
-      }));
-      $scope.updateTeam = function(teamName) {
-        $scope.selectedTeam = _.filter($scope.people, function (person){
-          return person.currentTeam === teamName;
-        })
-      };
+    });
+    SelfAssessment.getTeams().then(function (teams) {
+      $scope.teams = teams;
     });
   });
