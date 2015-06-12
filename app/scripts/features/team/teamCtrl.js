35664c473e3aaf50c027d158e0ee4092568ad20a
Rouan <rmw@thoughtworks.com>
2015-06-12 14:27:33 +0200

    autoselect first team and add rubric to every page

--- app/scripts/features/team/teamCtrl.js
+++ app/scripts/features/team/teamCtrl.js
@@ -4,5 +4,6 @@ angular.module('coachingApp')
   .controller('TeamCtrl', function ($scope, SelfAssessment) {
     SelfAssessment.getTeams().then(function (teams) {
       $scope.teams = teams;
+      $scope.selectedTeam = teams[0];
     });
   });
